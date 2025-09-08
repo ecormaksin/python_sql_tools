e641b9f7cc1c81aeec98dacfda1065294d8b737f
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2025-09-09 00:43:49 +0900

    MySQL カラム定義取得用SQLを追加

---
+++ sample/mysql/get_schema.sql
@@ -0,0 +1,27 @@
+@set table_name = ''
+
+select
+	sq_a.table_name,
+	sq_a.column_name,
+	sq_a.COLUMN_COMMENT,
+	sq_a.IS_NULLABLE,
+	sq_a.COLUMN_TYPE,
+	case 
+		when sq_b.ORDINAL_POSITION is null then ''
+		else sq_b.ORDINAL_POSITION
+	end as `KEY_POSITION`
+from
+	information_schema.`COLUMNS` sq_a
+	left outer join information_schema.KEY_COLUMN_USAGE sq_b
+		on 
+		sq_b.TABLE_SCHEMA = sq_a.TABLE_SCHEMA 
+		and sq_b.TABLE_NAME = sq_a.TABLE_NAME 
+		and sq_b.CONSTRAINT_NAME = 'PRIMARY'
+		and sq_b.column_name = sq_a.column_name
+where
+	sq_a.TABLE_SCHEMA = 'dbeaver_sample'
+	and ('' = :table_name or sq_a.table_name = :table_name)
+order by
+	sq_a.table_name,
+	sq_a.ORDINAL_POSITION
+;
