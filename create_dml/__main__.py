39d525ba5c90c087f1e56b515d54da9ccf0c7592
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2025-10-02 00:06:47 +0900

    オブジェクト生成に関する処理を__main__.pyからアプリケーション クラスへ移動

--- create_dml/__main__.py
+++ create_dml/__main__.py
@@ -1,9 +1,4 @@
 import argparse
-import datetime
-import sys
-import tempfile
-import traceback
-from pathlib import Path
 
 from rich_argparse import RichHelpFormatter
 
@@ -11,13 +6,6 @@ from shared_code.application.dml.dml_files_creator import (
     DMLFilesCreationRequest,
     DMLFilesCreator,
 )
-from shared_code.domain.sink_dml_dir_path import SinkDMLDirectoryPath
-from shared_code.domain.source_data_xlsx_file_path import SourceDataXlsxFilePath
-from shared_code.infra.file_system.app_config_jsonc_file_reader import (
-    AppConfigJsoncFileReader,
-)
-from shared_code.infra.file_system.directory_creator import DirectoryCreator
-from shared_code.infra.file_system.file_existence_checker import FileExistenceChecker
 
 
 def create_dml():
@@ -41,46 +29,15 @@ def create_dml():
 
     args = arg_parser.parse_args()
 
-    src_xlsx_file_path_str = args.source
-    if args.destination:
-        sink_dml_dir_path_str = args.destination
-    else:
-        now = datetime.datetime.now()
-        sink_dml_dir_path_str = str(
-            Path(tempfile.gettempdir())
-            .joinpath("python-sql-tools")
-            .joinpath("dml")
-            .joinpath(now.strftime("%Y%m%d-%H%M%S"))
-        )
-
-    if args.config:
-        config_file_path = args.config
-    else:
-        config_file_path = Path(__file__).parent.parent.joinpath("app_config.json")
-
-    if FileExistenceChecker.not_exists(file_path=src_xlsx_file_path_str):
-        print(f"Source file '{src_xlsx_file_path_str}' not found.")
-        sys.exit(1)
-
-    try:
-        DirectoryCreator.execute(path_str=sink_dml_dir_path_str)
-    except IOError | PermissionError:
-        stacktrace_str = traceback.format_exc()
-        print(
-            f"Cannot create dml output directory '{sink_dml_dir_path_str}'. detail: {stacktrace_str}"
-        )
-        sys.exit(1)
-
-    app_config = AppConfigJsoncFileReader.execute(file_path=config_file_path)
-
     a_request = DMLFilesCreationRequest(
-        source_data_xlsx_file_path=SourceDataXlsxFilePath(value=src_xlsx_file_path_str),
-        sink_dml_dir_path=SinkDMLDirectoryPath(value=sink_dml_dir_path_str),
-        app_config=app_config,
+        source_data_xlsx_file_path_str=args.source,
+        sink_dml_dir_path_str=args.destination,
+        app_config_file_path_str=args.config,
     )
 
-    DMLFilesCreator.execute(a_request=a_request)
+    a_response = DMLFilesCreator.execute(a_request=a_request)
 
+    sink_dml_dir_path_str = a_response.sink_dml_dir_path_str
     print(f"DML files are created at '{sink_dml_dir_path_str}'.")
 
 
--- shared_code/application/dml/dml_files_creator.py
+++ shared_code/application/dml/dml_files_creator.py
@@ -1,4 +1,9 @@
+import datetime
+import tempfile
+import traceback
 from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional
 
 from shared_code.application.dml.all_tables_dmls_builder import (
     AllTablesDMLsBuilder,
@@ -11,24 +16,43 @@ from shared_code.application.dml.all_tables_dmls_writer import (
 from shared_code.domain.app_config import AppConfig
 from shared_code.domain.sink_dml_dir_path import SinkDMLDirectoryPath
 from shared_code.domain.source_data_xlsx_file_path import SourceDataXlsxFilePath
+from shared_code.infra.file_system.app_config_jsonc_file_reader import (
+    AppConfigJsoncFileReader,
+)
+from shared_code.infra.file_system.directory_creator import DirectoryCreator
+from shared_code.infra.file_system.file_existence_checker import FileExistenceChecker
 
 
 @dataclass(frozen=True)
 class DMLFilesCreationRequest:
-    source_data_xlsx_file_path: SourceDataXlsxFilePath
-    sink_dml_dir_path: SinkDMLDirectoryPath
-    app_config: AppConfig
+    source_data_xlsx_file_path_str: str  # SourceDataXlsxFilePath
+    sink_dml_dir_path_str: Optional[str]  # SinkDMLDirectoryPath
+    app_config_file_path_str: Optional[str]  # AppConfig
+
+
+@dataclass(frozen=True)
+class DMLFilesCreationResponse:
+    sink_dml_dir_path_str: str
 
 
 class DMLFilesCreator:
     @classmethod
-    def execute(cls, a_request: DMLFilesCreationRequest):
-        app_config = a_request.app_config
-        number_of_lines_per_file = app_config.number_of_lines_per_file
+    def execute(cls, a_request: DMLFilesCreationRequest) -> DMLFilesCreationResponse:
+        source_data_xlsx_file_path = DMLFilesCreator.__get_source_data_xlsx_file_path(
+            file_path_str=a_request.source_data_xlsx_file_path_str
+        )
+
+        sink_dml_dir_path = DMLFilesCreator.__get_sink_dml_dir_path(
+            dir_path_str=a_request.sink_dml_dir_path_str
+        )
+
+        app_config = DMLFilesCreator.__get_app_config(
+            file_path_str=a_request.app_config_file_path_str
+        )
 
         all_tables_dmls_build_request = AllTablesDMLsBuildRequest(
-            source_data_xlsx_file_path=a_request.source_data_xlsx_file_path,
-            app_config=a_request.app_config,
+            source_data_xlsx_file_path=source_data_xlsx_file_path,
+            app_config=app_config,
         )
 
         all_tables_dmls = AllTablesDMLsBuilder.execute(
@@ -36,10 +60,55 @@ class DMLFilesCreator:
         )
 
         all_tables_dmls_write_request = AllTablesDMLsWriteRequest(
-            sink_dml_dir_path=a_request.sink_dml_dir_path,
+            sink_dml_dir_path=sink_dml_dir_path,
             all_tables_dmls=all_tables_dmls,
-            number_of_lines_per_file=number_of_lines_per_file,
+            number_of_lines_per_file=app_config.number_of_lines_per_file,
             file_name_prefix=app_config.file_name_prefix,
         )
 
         AllTablesDMLsWriter.execute(a_request=all_tables_dmls_write_request)
+
+        return DMLFilesCreationResponse(sink_dml_dir_path_str=sink_dml_dir_path.value)
+
+    @classmethod
+    def __get_source_data_xlsx_file_path(
+        cls, file_path_str: str
+    ) -> SourceDataXlsxFilePath:
+        if FileExistenceChecker.not_exists(file_path=file_path_str):
+            raise RuntimeError(f"Source file '{file_path_str}' not found.")
+
+        return SourceDataXlsxFilePath(value=file_path_str)
+
+    @classmethod
+    def __get_sink_dml_dir_path(
+        cls, dir_path_str: Optional[str]
+    ) -> SinkDMLDirectoryPath:
+        sink_dml_dir_path_str = dir_path_str
+        if not sink_dml_dir_path_str:
+            now = datetime.datetime.now()
+            sink_dml_dir_path_str = str(
+                Path(tempfile.gettempdir())
+                .joinpath("python-sql-tools")
+                .joinpath("dml")
+                .joinpath(now.strftime("%Y%m%d-%H%M%S"))
+            )
+
+        try:
+            DirectoryCreator.execute(path_str=sink_dml_dir_path_str)
+        except IOError | PermissionError:
+            stacktrace_str = traceback.format_exc()
+            raise RuntimeError(
+                f"Cannot create dml output directory '{sink_dml_dir_path_str}'. detail: {stacktrace_str}"
+            )
+
+        return SinkDMLDirectoryPath(value=sink_dml_dir_path_str)
+
+    @classmethod
+    def __get_app_config(cls, file_path_str: Optional[str]) -> AppConfig:
+        config_file_path = file_path_str
+        if not config_file_path:
+            config_file_path = Path(__file__).parent.parent.parent.parent.joinpath(
+                "app_config.json"
+            )
+
+        return AppConfigJsoncFileReader.execute(file_path=config_file_path)
