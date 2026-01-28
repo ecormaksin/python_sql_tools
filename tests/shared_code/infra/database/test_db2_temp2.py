99eab434ecb9e844a44b6e640d0f261fd1e577e1
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2026-01-28 23:19:26 +0900

    WIP

---
+++ tests/shared_code/infra/database/test_db2_temp2.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine, text
+from testcontainers.mssql import SqlServerContainer
+from testcontainers.db2 import Db2Container
+
+from tests.shared_code.infra.database.sqlalchemy.orm.entity_creator import EntityCreator
+
+
+class TestClass:
+    def test(self):
+        url = "db2+ibm_db://db2inst1:password@localhost:50000/testdb"
+
+        engine = create_engine(url, echo=True)
+
+        EntityCreator.execute(engine=engine)
+
+        with engine.connect() as connection:
+            result = connection.execute(
+                text("SELECT TABNAME FROM SYSCAT.TABLES")
+            )
+            print(result.all())
