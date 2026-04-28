a5d624c069b5a19b6fbedb6de399011e9a06eb89
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2026-04-28 21:32:54 +0900

    WIP

--- sample/docker/mysql/init/setup.sh
+++ sample/docker/mysql/init/setup.sh
@@ -3,17 +3,29 @@
 BASE_DIR=/setup
 CONF_FILE_PATH=${BASE_DIR}/my.cnf
 
-mysql --defaults-extra-file=${CONF_FILE_PATH} < ${BASE_DIR}/01.create_databases.sql
+declare -a databases=("artist" "genre" "media_type" "album" "play_list" "employee" "customer" "invoice")
+
+TABLE_DDL_DIR=${BASE_DIR}/01.create_databases
+i=1
+for database in "${databases[@]}"; do
+  num=$(printf "%02d" "$i")
+  file_name="${num}.$database.sql"
+
+  mysql --defaults-extra-file="${CONF_FILE_PATH}" < "${TABLE_DDL_DIR}"/"${file_name}"
+
+  i=$((i + 1))
+done
 
 TABLE_DDL_DIR=${BASE_DIR}/02.create_tables
-mysql --defaults-extra-file=${CONF_FILE_PATH} --database=artist < ${TABLE_DDL_DIR}/01.artist.sql
-mysql --defaults-extra-file=${CONF_FILE_PATH} --database=genre < ${TABLE_DDL_DIR}/02.genre.sql
-mysql --defaults-extra-file=${CONF_FILE_PATH} --database=media_type < ${TABLE_DDL_DIR}/03.media_type.sql
-mysql --defaults-extra-file=${CONF_FILE_PATH} --database=album < ${TABLE_DDL_DIR}/04.album.sql
-mysql --defaults-extra-file=${CONF_FILE_PATH} --database=play_list < ${TABLE_DDL_DIR}/05.play_list.sql
-mysql --defaults-extra-file=${CONF_FILE_PATH} --database=employee < ${TABLE_DDL_DIR}/06.employee.sql
-mysql --defaults-extra-file=${CONF_FILE_PATH} --database=customer < ${TABLE_DDL_DIR}/07.customer.sql
-mysql --defaults-extra-file=${CONF_FILE_PATH} --database=invoice < ${TABLE_DDL_DIR}/08.invoice.sql
+i=1
+for database in "${databases[@]}"; do
+  num=$(printf "%02d" "$i")
+  file_name="${num}.$database.sql"
+
+  mysql --defaults-extra-file="${CONF_FILE_PATH}" --database="${database}" < "${TABLE_DDL_DIR}"/"${file_name}"
+
+  i=$((i + 1))
+done
 
 VIEW_DDL_DIR=${BASE_DIR}/03.create_views
 mysql --defaults-extra-file=${CONF_FILE_PATH} --database=album < ${VIEW_DDL_DIR}/01.album.sql
