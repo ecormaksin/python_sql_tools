ad448a2db3421432955a5d07d6757dc85d7d459d
Makoto Oda <truth_jp_4133@yahoo.co.jp>
2026-05-05 17:31:22 +0900

    WIP

--- tests/shared_code/infra/database/db_container_for_test.py
+++ tests/shared_code/infra/database/db_container_for_test.py
@@ -8,9 +8,13 @@ from testcontainers.mysql import MySqlContainer
 from testcontainers.postgres import PostgresContainer
 
 from shared_code.infra.database.rdbms_type import RDBMSType
-from shared_code.infra.database.sqlalchemy_sql_file_executor import SQLAlchemySQLFileExecutor
+from shared_code.infra.database.sqlalchemy_sql_file_executor import (
+    SQLAlchemySQLFileExecutor,
+)
 from tests.project_root_resolver import ProjectRootResolver
-from tests.shared_code.infra.database.ddl_file_name_control_entity import DDLFileNameControlEntityList
+from tests.shared_code.infra.database.ddl_file_name_control_entity import (
+    DDLFileNameControlEntityList,
+)
 from tests.shared_code.infra.database.rdbms_type_for_test import RDBMSTypeForTest
 
 
@@ -41,58 +45,66 @@ class DbContainerForTest:
         view_ddl_relative_dirs.append("03.create_views")
         view_ddl_dir = project_root.joinpath(*view_ddl_relative_dirs)
 
-        db_container = None
-
-        if rdbms_type == RDBMSType.MYSQL:
-            db_container = MySqlContainer(
-                image="mysql:9.5.0-oraclelinux9", username=rdbms_type_for_test.root_username,
-                root_password=rdbms_type_for_test.root_password
-            )
-
-        if rdbms_type == RDBMSType.POSTGRES:
-            db_container = PostgresContainer("postgres:18.1", username=rdbms_type_for_test.root_username,
-                                             password=rdbms_type_for_test.root_password
-                                             )
+        db_container = rdbms_type_for_test.db_container
 
         if not db_container:
-            raise RuntimeError(f"'{rdbms_type.key}' に対応するRDBMSの種類を指定してください。")
+            raise RuntimeError(
+                f"'{rdbms_type.key}' に対応するRDBMSの種類を指定してください。"
+            )
 
         db_container.start()
         self._db_container = db_container
 
         test_db_url = db_container.get_connection_url()
-        logger.info(test_db_url)  # mysql://test:test@localhost:32769/test
+        logger.info(test_db_url)
         test_db_scheme_replaced_url = rdbms_type.replace_scheme(url=test_db_url)
+        original_db_engine = create_engine(test_db_scheme_replaced_url)
+        original_db_connection = original_db_engine.connect()
+        self._original_db_connection = original_db_connection
 
-        for serial_number, ctrl_entity in enumerate(DDLFileNameControlEntityList.get(), start=1):
+        for serial_number, ctrl_entity in enumerate(
+            DDLFileNameControlEntityList.get(), start=1
+        ):
             schema_name = ctrl_entity.schema_name
             logger.info("")
             logger.info(f"====={schema_name}")
 
             ddl_file_name = ctrl_entity.get_file_name(serial_number=serial_number)
 
-            url = re.sub("/test$", f"/{schema_name}", test_db_url)
+            url = test_db_url
+            if rdbms_type == RDBMSType.MYSQL:
+                url = re.sub("/test$", f"/{schema_name}", url)
             scheme_replaced_url = rdbms_type.replace_scheme(url=url)
 
-            if not database_exists(scheme_replaced_url):
-                original_db_engine = create_engine(test_db_scheme_replaced_url)
-                with original_db_engine.connect() as connection:
-                    schema_ddl_file = schema_ddl_dir.joinpath(ddl_file_name)
-                    SQLAlchemySQLFileExecutor.execute(file_path=schema_ddl_file, connection=connection)
+            if rdbms_type_for_test.check_database_exists(
+                url=scheme_replaced_url,
+                connection=original_db_connection,
+                schema_name=schema_name,
+            ):
+                schema_ddl_file = schema_ddl_dir.joinpath(ddl_file_name)
+                SQLAlchemySQLFileExecutor.execute(
+                    file_path=schema_ddl_file, connection=original_db_connection
+                )
 
             engine = create_engine(scheme_replaced_url)
 
-            with engine.connect() as connection:
+            with engine.connect() as db_connection:
                 table_ddl_file = table_ddl_dir.joinpath(ddl_file_name)
-                SQLAlchemySQLFileExecutor.execute(file_path=table_ddl_file, connection=connection)
+                SQLAlchemySQLFileExecutor.execute(
+                    file_path=table_ddl_file, connection=db_connection
+                )
 
                 if ctrl_entity.view:
                     view_ddl_file_name = ctrl_entity.get_view_file_name()
                     view_ddl_file = view_ddl_dir.joinpath(view_ddl_file_name)
-                    SQLAlchemySQLFileExecutor.execute(file_path=view_ddl_file, connection=connection)
+                    SQLAlchemySQLFileExecutor.execute(
+                        file_path=view_ddl_file, connection=db_connection
+                    )
 
                 query = rdbms_type_for_test.entities_get_query
-                result = connection.execute(text(query), {"schema_name": schema_name})
+                result = db_connection.execute(
+                    text(query), {"schema_name": schema_name}
+                )
                 for row in result:
                     logger.info(f"{row.table_type}: {row.table_name}")
 
@@ -100,6 +112,9 @@ class DbContainerForTest:
         return self
 
     def __exit__(self, exc_type, exc_value, a_traceback):
+        if self._original_db_connection:
+            self._original_db_connection.close()
+
         if self._db_container:
             self._db_container.stop()
 
--- tests/shared_code/infra/database/rdbms_type_for_test.py
+++ tests/shared_code/infra/database/rdbms_type_for_test.py
@@ -1,12 +1,52 @@
 from enum import Enum
 
+from sqlalchemy import text
+from sqlalchemy.engine.base import Connection
+from sqlalchemy_utils import database_exists
+from testcontainers.core.generic import DbContainer
+from testcontainers.mysql import MySqlContainer
+from testcontainers.postgres import PostgresContainer
+
 from shared_code.infra.database.rdbms_type import RDBMSType
 
 
+def check_database_exists_by_url_only(
+    url: str, connection: Connection, schema_name: str
+) -> bool:
+    return database_exists(url)
+
+
+def check_database_exists_for_postgres(
+    url: str, connection: Connection, schema_name: str
+) -> bool:
+    query = """
+    select 
+        schema_name
+    from
+        information_schema.schemata
+    where 
+        schema_name = :schema_name
+    """
+
+    result = connection.execute(text(query), {"schema_name": schema_name}).fetchall()
+
+    return len(result) > 0
+
+
 class RDBMSTypeForTest(Enum):
-    DB2 = (RDBMSType.DB2, "", "", "")
-    MSSQL = (RDBMSType.MSSQL, "", "", "")
-    MYSQL = (RDBMSType.MYSQL, "root", "test", """
+    # DB2 = (RDBMSType.DB2, "", "", "")
+    # MSSQL = (RDBMSType.MSSQL, "", "", "")
+    # ORACLE = (RDBMSType.ORACLE, "", "", "")
+    MYSQL = (
+        RDBMSType.MYSQL,
+        MySqlContainer(
+            image="mysql:9.5.0-oraclelinux9",
+            username="root",
+            root_password="test",
+            password="test",
+        ),
+        check_database_exists_by_url_only,
+        """
 SELECT 
     case 
         when TABLE_TYPE = 'BASE TABLE' then 'table'
@@ -25,9 +65,17 @@ order by
         else 3
     end,
     TABLE_NAME    
-""")
-    ORACLE = (RDBMSType.ORACLE, "", "", "")
-    POSTGRES = (RDBMSType.POSTGRES, "postgres", "test", """
+""",
+    )
+    POSTGRES = (
+        RDBMSType.POSTGRES,
+        PostgresContainer(
+            "postgres:18.1",
+            username="postgres",
+            password="test",
+        ),
+        check_database_exists_for_postgres,
+        """
 SELECT 
     case 
         when TABLE_TYPE = 'BASE TABLE' then 'table'
@@ -48,12 +96,19 @@ order by
         else 3
     end,
     TABLE_NAME    
-""")
+""",
+    )
 
-    def __init__(self, rdbms_type: RDBMSType, root_username: str, root_password: str, entities_get_query: str):
+    def __init__(
+        self,
+        rdbms_type: RDBMSType,
+        db_container: DbContainer,
+        check_database_exists_func,
+        entities_get_query: str,
+    ):
         self._rdbms_type = rdbms_type
-        self._root_username = root_username
-        self._root_password = root_password
+        self._db_container = db_container
+        self._check_database_exists_func = check_database_exists_func
         self._entities_get_query = entities_get_query
 
     @classmethod
@@ -68,13 +123,14 @@ order by
         return self._rdbms_type
 
     @property
-    def root_username(self) -> str:
-        return self._root_username
-
-    @property
-    def root_password(self) -> str:
-        return self._root_password
+    def db_container(self) -> DbContainer:
+        return self._db_container
 
     @property
     def entities_get_query(self) -> str:
         return self._entities_get_query
+
+    def check_database_exists(
+        self, url: str, connection: Connection, schema_name: str
+    ) -> bool:
+        return self._check_database_exists_func(url, connection, schema_name)
